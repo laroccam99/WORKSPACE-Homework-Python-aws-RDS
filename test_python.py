5f169cb1d92fcc00746514e7a5ef185de13a6f09
laroccam99 <laroccam99@gmail.com>
2024-01-11 11:55:00 +0100

    aggiunta test connessione

--- test_python.py
+++ test_python.py
@@ -1,25 +1,41 @@
+#import pymysql
+import boto3
+import os
+
 import random
 from datetime import datetime, timedelta
-count = 0
-def generate_random_tuple(count):
-            #count = count+1
-            speed = round(random.uniform(1.0, 3.0), 2)
-            rotation = round(random.uniform(10.0, 90.0), 2)
-            date_time = datetime.now() - timedelta(days=random.randint(1, 365), hours=random.randint(1, 24))
-            date_time_str = date_time.strftime('%Y-%m-%d %H:%M:%S')
-            power = random.randint(50, 200)
-            return (count, speed, rotation, date_time_str, power)
 
-def generate_random_tuples(num_tuples, records_to_insert, count):
-    for _ in range(num_tuples - len(records_to_insert)):
-        records_to_insert.append(generate_random_tuple(count))
-        count=count+1
+import mysql.connector
 
-    return records_to_insert
+ENDPOINT='tutorial-database-1.cjug0u60whdi.eu-central-1.rds.amazonaws.com'
+PORT=3306
+USER='admin'
+PASSWORD='password'
+REGION='eu-central-1'
+DBNAME='tutorial-database-1'
+SSLCERTIFICATE = 'global-bundle.pem'
 
-num_tuples_to_generate = 50  #Numero desiderato di tuple
-records_to_insert = []
-generated_tuples = generate_random_tuples(num_tuples_to_generate, records_to_insert, count)
+os.environ['LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN'] = '1'
 
-for record in generated_tuples:
-    print(record)
+#gets the credentials from .aws/credentials
+#session = boto3.Session(profile_name='default')
+#client = session.client('rds')
+#print("Access key: "+ session.get_credentials().access_key)
+#print("Secret key: " + session.get_credentials().secret_key)
+
+#token = client.generate_db_auth_token(DBHostname=ENDPOINT, Port=PORT, DBUsername=USER, Region=REGION)    
+#print("token: " + token)
+
+#connection = pymysql.connect(host=ENDPOINT, user=USER, passwd=PASSWORD, port=PORT, database=DBNAME, connect_timeout=10, ssl_ca='global-bundle.pem')
+connection = mysql.connector.connect(host=ENDPOINT, user=USER, password=PASSWORD, database=DBNAME, port=PORT)
+print("Connessione al database riuscita!")
+
+try:
+    if connection.is_connected():
+        print(f"Connessione al database {DBNAME} riuscita.")
+except mysql.connector.Error as e:
+        print(f"Errore durante la connessione al database: {e}")
+finally:
+    if connection.is_connected():
+        connection.close()
+        print("Connessione al database chiusa.")
